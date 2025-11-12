d9b72277e7979706d41590531a6384261a80c5f4
Samuel <samuel_mesquita@estudante.sesisenai.org.br>
2025-11-11 21:48:33 -0300

    mais um commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/* PESQUISAR PARA CRIAR ANIMAÇÕES DEPOIS */
+
+function construct(){
+    window.location.href = "construct.html";
+}
+
+function unity(){
+    window.location.href = "unity.html";
+}
+
+function unreal(){
+    window.location.href = "unreal.html";
+}
+
+function piskelapp(){
+    window.location.href = "piskelapp.html";
+}
+
+function blender(){
+    window.location.href = "blender.html";
+}
